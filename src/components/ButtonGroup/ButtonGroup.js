e6e2885c43f8d9eab7ffbbd9bcf7e2737a97c03e
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-06 13:36:15 +0400

    use React.cloneElement to add new className to each child

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -3,12 +3,28 @@ import PropTypes from "prop-types";
 
 import "./style.css"
 
+//we run through each element in children
+//and clone it and return it by adding className 'ButtonGroup__item' to each child
 const ButtonGroup = ({children}) => {
-    return children;
+    return (
+        <div
+            className="ButtonGroup">
+            {React.Children.map(children,(child) => (
+                React.cloneElement(
+                    child,
+                    {className: 'ButtonGroup__item'}
+                )
+            ))}
+        </div>
+    )
 };
 
 ButtonGroup.propTypes = {
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]).isRequired,
 };
 
+
 export default ButtonGroup;
+
+//in order to add new element to children use React.cloneElement
+//as it will work even though you have one child only
